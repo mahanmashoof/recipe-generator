6884955577109a3a1c10259eb3339f306479e2d8
mahanmashoof <mahan.mashoof@gmail.com>
2025-08-15 14:20:04 +0200

    add chat service

---
+++ src/main/java/com/training/recipe_generator/ChatService.java
@@ -0,0 +1,15 @@
+package com.training.recipe_generator;
+
+import org.springframework.ai.chat.model.ChatModel;
+
+public class ChatService {
+    private final ChatModel chatModel;
+
+    public ChatService(ChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    public String getResponse(String prompt) {
+        return chatModel.call(prompt);
+    }
+}
