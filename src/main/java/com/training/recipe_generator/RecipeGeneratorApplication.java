895f029a2fd7a927ab65beee72d01e26661dfafd
mahanmashoof <mahan.mashoof@gmail.com>
2025-08-15 13:26:14 +0200

    first

---
+++ src/main/java/com/training/recipe_generator/RecipeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.training.recipe_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeGeneratorApplication.class, args);
+	}
+
+}
